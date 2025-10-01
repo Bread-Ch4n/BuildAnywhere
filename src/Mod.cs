97ba2bb76775f0556c5ede526da1c2bb0ee0b47d
Bread-Ch4n <94946192+Bread-Ch4n@users.noreply.github.com>
2025-10-01 20:16:13 +0300

    feat(v1.0.0): initial release

---
+++ src/Mod.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+using BuildAnywhere;
+using HarmonyLib;
+using Il2CppMonomiPark.SlimeRancher.Player.PlayerItems;
+using Il2CppMonomiPark.SlimeRancher.World;
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(Mod), "BuildAnywhere", "1.0.0", "Bread-Chan", "")]
+[assembly: MelonGame("MonomiPark", "SlimeRancher2")]
+
+namespace BuildAnywhere;
+
+public class Mod : MelonMod
+{
+    #region Setup
+
+    public override void OnInitializeMelon()
+    {
+        var h = new HarmonyLib.Harmony("com.bread-chan.build_anywhere");
+        h.PatchAll(typeof(Mod));
+
+        MelonLogger.Msg("Initialized.");
+    }
+
+    #endregion
+
+    #region Patches
+
+    [HarmonyPatch]
+    private class GadgetPatch
+    {
+        private static MethodBase TargetMethod() =>
+            AccessTools.Method(
+                typeof(Gadget),
+                "IsOverlapping",
+                new[]
+                {
+                    typeof(GadgetOverlapInfo).MakeByRefType(),
+                    typeof(float),
+                    typeof(LayerMask),
+                    typeof(bool),
+                }
+            );
+
+        [HarmonyPostfix]
+        private static void Postfix(ref bool __result) => __result = false;
+    }
+
+    [HarmonyPatch(typeof(Gadget), "IsOverlapping", typeof(float), typeof(LayerMask), typeof(bool))]
+    [HarmonyPostfix]
+    private static void Gadget_IsOverlapping(ref bool __result) => __result = false;
+
+    [HarmonyPatch(typeof(GadgetItem), nameof(GadgetItem.IsPlacementValid))]
+    [HarmonyPostfix]
+    private static void GadgetItem_IsPlacementValid(GadgetItem __instance, ref bool __result)
+    {
+        __instance.SetGadgetPlacementValidity(true);
+        __result = true;
+    }
+
+    #endregion
+}
